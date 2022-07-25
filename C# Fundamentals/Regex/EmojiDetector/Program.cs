160202b5141492dfab82cd80cf299465be1a074d
Kristina <kristina.hmn@gmail.com>
2022-07-25 17:51:55 +0300

    Added more Regex exercises

---
+++ C# Fundamentals/Regex/EmojiDetector/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EmojiDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string onlyNumbers = new String(input.Where(Char.IsDigit).ToArray());
+
+            int[] numericValues = onlyNumbers.ToArray().Select(c => (int)char.GetNumericValue(c)).ToArray();
+
+            long coolThershold = 1;
+
+            for (int i = 0; i < numericValues.Length; i++)
+            {
+                coolThershold *= numericValues[i];
+            }
+            Console.WriteLine($"Cool threshold: {coolThershold}");
+
+            Regex pattern = new Regex(@"([\:]{2}|[\*]{2})(?<emoji>[A-Z]{1}[a-z]{2,})(\1)");
+
+            MatchCollection matches = pattern.Matches(input);
+            List<string> emojis = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                string emoji = match.Groups["emoji"].Value;
+                emojis.Add(emoji);
+            }
+            Console.WriteLine($"{emojis.Count} emojis found in the text. The cool ones are:");
+
+            Dictionary<string, int> emojiCoolness = new Dictionary<string, int>();
+
+            foreach (string emoj in emojis)
+            {
+                int sumOfASCII = 0;
+
+                foreach (char charact in emoj)
+                {
+                    sumOfASCII += (int)charact;
+                }
+                emojiCoolness.Add(emoj, sumOfASCII);
+            }
+
+            List<string> coolEmojis = new List<string>();   
+            foreach (var kvp in emojiCoolness)
+            {
+                if (kvp.Value >= coolThershold)
+                {
+                    coolEmojis.Add(kvp.Key);
+                }
+            }
+
+            List<string> forPrinting = new List<string>();
+            MatchCollection matches2 = pattern.Matches(input);
+            foreach (Match match in matches2)
+            {
+                string comparison = match.Groups["emoji"].Value;
+                foreach (var item in coolEmojis)
+                {
+                    if (comparison == item)
+                    {
+                        string emoji = match.Groups[0].Value;
+                        forPrinting.Add(emoji);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join("\n", forPrinting));
+        }
+    }
+}
---
+++ C# Fundamentals/Regex/FancyBarcode/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FancyBarcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int barcodeCount = int.Parse(Console.ReadLine());
+            string barcodePattern = @"@#+[A-Z][a-zA-Z\d]{4,}[A-Z]@#+";
+
+            for (int i = 0; i < barcodeCount; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (Regex.IsMatch(input, barcodePattern))
+                {
+                    char[] digits = input.Where(x => char.IsDigit(x)).ToArray();
+
+                    if (digits.Length == 0)
+                    {
+                        Console.WriteLine("Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {string.Join(string.Empty, digits)}");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}
