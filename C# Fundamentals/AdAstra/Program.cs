9a9d8554f462cd222b19e273f2b58c5519a83506
Kristina <kristina.hmn@gmail.com>
2022-07-25 17:43:57 +0300

    Added Regex exercise

---
+++ C# Fundamentals/AdAstra/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AdAstra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(@"(\#|\|)(?<item>[A-Za-z\s]+)(\1)(?<date>\d{2}\/\d{2}\/\d{2})(\1)(?<calories>\d+|(\d+)?\.(\d+)?)(\1)");
+
+            MatchCollection matches = regex.Matches(input);
+
+            int allCalories = 0;
+
+            foreach (Match match in matches)
+            {
+                string item = match.Groups["item"].Value;
+                string date = match.Groups["date"].Value;
+                int calories = int.Parse(match.Groups["calories"].Value);
+                allCalories+= calories;
+            }
+            // for a single day I need 2000
+            int days = 0;
+            while (allCalories >= 2000)
+            {
+                allCalories -= 2000;
+                days++;
+            }
+            Console.WriteLine($"You have food to last you for: {days} days!");
+
+            foreach (Match match in matches)
+            {
+                string item = match.Groups["item"].Value;
+                string date = match.Groups["date"].Value;
+                int calories = int.Parse(match.Groups["calories"].Value);
+
+                Console.WriteLine($"Item: {item}, Best before: {date}, Nutrition: {calories}");
+            }
+        }
+    }
+}
