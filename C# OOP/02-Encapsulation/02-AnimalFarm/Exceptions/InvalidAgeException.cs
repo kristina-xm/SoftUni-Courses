76aeb29bad98de608904e73ea30d219b6c3eb575
Kristina <kristina.hmn@gmail.com>
2022-07-28 17:00:41 +0300

    Added solution to Encapsulation exercise

---
+++ C# OOP/02-Encapsulation/02-AnimalFarm/Exceptions/InvalidAgeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalFarm.Exceptions
+{
+    public class InvalidAgeException : Exception
+    {
+        private const string message = "Age should be between 0 and 15.";
+
+        public InvalidAgeException() : base(message)
+        {
+
+        }
+    }
+}
---
+++ C# OOP/02-Encapsulation/02-AnimalFarm/Exceptions/InvalidNameException.cs
@@ -0,0 +1,16 @@
+﻿namespace AnimalFarm.Exceptions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class InvalidNameException : Exception
+    {
+        internal const string message = "Name cannot be empty.";
+        public InvalidNameException() : base(message) 
+        {
+            
+        }
+
+    }
+}
