bb52b99e0a1e3fc30cde4078e70ca21211d417e4
Kristina <kristina.hmn@gmail.com>
2022-08-23 16:07:21 +0300

    Renamed folder

---
+++ Practice/C# OOP/Exam 01/Structure and Business Logic/Core/Controller.cs
@@ -0,0 +1,176 @@
+﻿namespace NavalVessels.Core
+{
+    using NavalVessels.Core.Contracts;
+    using NavalVessels.Models;
+    using NavalVessels.Models.Contracts;
+    using NavalVessels.Repositories;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Controller : IController
+    {
+        private VesselRepository vessels;
+        private List<ICaptain> captains;
+
+        public Controller()
+        {
+            vessels = new VesselRepository();
+            captains = new List<ICaptain>();
+        }
+
+
+        public string HireCaptain(string fullName)
+        {
+            var captain = new Captain(fullName);
+
+            if (captains.Any(c => c.FullName == fullName))
+            {
+                return $"Captain {fullName} is already hired.";
+            }
+
+            captains.Add(captain);
+            return $"Captain {fullName} is hired.";
+        }
+
+        public string ProduceVessel(string name, string vesselType, double mainWeaponCaliber, double speed)
+        {
+            IVessel vessel = null;
+
+            if (vessels.Models.Any(v => v.Name == name))
+            {
+                return $"{vesselType} vessel {name} is already manufactured.";
+            }
+
+            if (vesselType != "Submarine" && vesselType != "Battleship")
+            {
+                return "Invalid vessel type.";
+            }
+
+            if (vesselType == "Submarine")
+            {
+                vessel = new Submarine(name, mainWeaponCaliber, speed);
+            }
+            else if (vesselType == "Battleship")
+            {
+                vessel = new Battleship(name, mainWeaponCaliber, speed);
+            }
+
+            vessels.Add(vessel);
+
+            return $"{vesselType} {name} is manufactured with the main weapon caliber of {mainWeaponCaliber} inches and a maximum speed of {speed} knots.";
+        }
+
+        public string AssignCaptain(string selectedCaptainName, string selectedVesselName)
+        {
+            var captain = captains.FirstOrDefault(c => c.FullName == selectedCaptainName);
+            var vessel = vessels.FindByName(selectedVesselName);
+
+            if (captain == null)
+            {
+                return $"Captain {selectedCaptainName} could not be found.";
+            }
+
+            if (vessel == null)
+            {
+                return $"Vessel {selectedVesselName} could not be found.";
+            }
+
+            if (vessel.Captain != null)
+            {
+                return $"Vessel {selectedVesselName} is already occupied.";
+            }
+
+            captain.AddVessel(vessel);
+            vessel.Captain = captain;
+
+            return $"Captain {selectedCaptainName} command vessel {selectedVesselName}.";
+        }
+
+        public string AttackVessels(string attackingVesselName, string defendingVesselName)
+        {
+            var attackingVessel = vessels.Models.FirstOrDefault(x => x.Name == attackingVesselName);
+
+            var defendingVessel = vessels.Models.FirstOrDefault(x => x.Name == defendingVesselName);
+
+            if (attackingVessel == null)
+            {
+                return $"Vessel {attackingVesselName} could not be found.";
+            }  
+            else if (defendingVessel == null)
+            {
+                return $"Vessel {defendingVesselName} could not be found.";
+            }  
+
+            if (attackingVessel.ArmorThickness == 0)
+            {
+                return $"Unarmored vessel {attackingVesselName} cannot attack or be attacked.";
+            }
+            else if (defendingVessel.ArmorThickness == 0)
+            {
+                return $"Unarmored vessel {defendingVesselName} cannot attack or be attacked.";
+            }
+
+            attackingVessel.Attack(defendingVessel);
+            attackingVessel.Captain.IncreaseCombatExperience();
+            defendingVessel.Captain.IncreaseCombatExperience();
+
+            return $"Vessel {defendingVesselName} was attacked by vessel {attackingVesselName} - current armor thickness: {defendingVessel.ArmorThickness}.";
+
+        }
+
+        public string CaptainReport(string captainFullName)
+        {
+            var captain = captains.FirstOrDefault(c => c.Vessels.Count > 0 && c.FullName == captainFullName);
+            
+            return captain.Report();
+        }
+
+        public string ServiceVessel(string vesselName)
+        {
+            var vessel = vessels.Models.FirstOrDefault(x => x.Name == vesselName);
+
+            if (vessel == null)
+            {
+                return $"Vessel {vesselName} could not be found.";
+            }
+
+            vessel.RepairVessel();
+            return $"Vessel {vesselName} was repaired.";
+        }
+
+        public string ToggleSpecialMode(string vesselName)
+        {
+            var vessel = vessels.Models.FirstOrDefault(x => x.Name == vesselName);
+
+            if (vessel == null)
+            {
+                return $"Vessel {vesselName} could not be found.";
+            }
+            if (vessel.GetType().Name == "Battleship")
+            {
+                Battleship currVesselType = (Battleship)(vessels.Models.FirstOrDefault(x => x.Name == vesselName));
+
+                currVesselType.ToggleSonarMode();
+
+                return $"Battleship {vesselName} toggled sonar mode.";
+            }
+            else
+            {
+                Submarine currVesselType = (Submarine)(vessels.Models.FirstOrDefault(x => x.Name == vesselName));
+
+                currVesselType.ToggleSubmergeMode();
+
+                return $"Submarine {vesselName} toggled submerge mode.";
+            }
+  
+        }
+
+        public string VesselReport(string vesselName)
+        {
+            var vessel = vessels.Models.FirstOrDefault(x => x.Name == vesselName);
+
+            return vessel.ToString();
+        }
+    }
+}
---
+++ Practice/C# OOP/Exam 01/Structure and Business Logic/Models/Captain.cs
@@ -0,0 +1,87 @@
+﻿namespace NavalVessels.Models
+{
+    using NavalVessels.Models.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Captain : ICaptain
+    {
+        public const int combatExperienceIncrease = 10;
+        private string fullName;
+
+        private int combatExperience = 0;
+        private List<IVessel> vessels;
+
+        public Captain(string name)
+        {
+            this.FullName = name;
+            vessels = new List<IVessel>();
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Captain full name cannot be null or empty string.");
+                }
+                fullName = value;
+            }
+        }
+
+        public int CombatExperience
+        {
+            get { return combatExperience; }
+            set { combatExperience = value; }
+        }
+
+        public ICollection<IVessel> Vessels => vessels;
+
+
+        public void AddVessel(IVessel vessel)
+        {
+            if (vessel == null)
+            {
+                throw new NullReferenceException("Null vessel cannot be added to the captain.");
+            }
+
+            Vessels.Add(vessel);
+        }
+
+        public void IncreaseCombatExperience()
+        {
+            this.CombatExperience += combatExperienceIncrease;
+        }
+
+        public string Report()
+        {
+            var output = $"{this.FullName} has {this.CombatExperience} combat experience and commands {this.vessels.Count} vessels.";
+
+            if (this.vessels.Count > 0)
+            {
+                var sb = new StringBuilder();
+
+                for (int i = 0; i < vessels.Count; i++)
+                {
+                    if (i == vessels.Count - 1)
+                    {
+                        sb.Append(vessels[i].ToString());
+                        break;
+                    }
+                    sb.AppendLine(vessels[i].ToString());
+                }
+                return $"{output}{Environment.NewLine}{sb.ToString()}";
+            }
+            else
+            {
+                return output;
+            }
+        }
+    }
+}
